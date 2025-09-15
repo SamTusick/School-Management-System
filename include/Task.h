24d4d015b69e08f63f839ee30d3e7f81f5fd8d0e
SamTusick <stusick@outlook.com>
2025-09-14 23:24:50 -0400

    Defined functions and defined task

--- include/Task.h
+++ include/Task.h
@@ -1,8 +1,29 @@
-//
-// Created by Stusi on 9/14/2025.
-//
-
+//Samuel Tusick
+//Task.h
+//This file contains declares all functions and defines struct
 #ifndef TASK_H
 #define TASK_H
+#include <vector>
+#include <string>
+
+
+// Task definintion
+
+struct Task
+{
+    int type;
+    std::string title;
+    std::string dueDate;
+    bool status;
+};
+
+
+//Functions Declarations
+
+void addTask(std::vector<Task>& tasks);
+void deleteTask(std::vector<Task>& tasks);
+void updateTask(std::vector<Task>& tasks);
+void changeStatus(std::vector<Task>& tasks);
+void viewTasks(std::vector<Task>& tasks);
 
 #endif //TASK_H
