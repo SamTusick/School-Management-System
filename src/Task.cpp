92733c4dacd90472c4b6546fe73a19dfe3231afb
SamTusick <stusick@outlook.com>
2025-09-15 14:09:09 -0400

    Started addTask function

--- src/Task.cpp
+++ src/Task.cpp
@@ -7,6 +7,38 @@
 
 void addTask(std::vector<Task>& tasks)
 {
+    int tasktype = 0;
+    Task newTask;
+
+    std::cout << "What is the type of your task?\n" << "1. Homework\n" <<
+        "2. Quiz\n" << "3. Exam\n" << "4. Other\n" << std::endl;
+    std::cin >> tasktype;
+
+    switch (tasktype)
+    {
+        case 1:
+            newTask.type = "Homework";
+            break;
+
+        case 2:
+            newTask.type = "Quiz";
+            break;
+
+        case 3:
+            newTask.type = "Exam";
+            break;
+
+        case 4:
+            newTask.type = "Other";
+            break;
+
+    default:
+            std:: cout << "Invalid Task Type!" << std::endl;
+            break;
+
+
+    }
+
 
 }
 
